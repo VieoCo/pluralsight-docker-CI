f6b620c0f32d3de1f15daff20fe10b53c9dfe26f
deyoungster <dela.deyoungster@gmail.com>
2017-08-21 08:50:38 -0700

    Final final Auto-CI to Docker test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Sample node.js web app for Pluralsight Docker CI course
-// For demonstration purposes only. >> Final Auto-CI to Docker test!
+// For demonstration purposes only. >> Final final Auto-CI to Docker test!
 'use strict';
 
 var express = require('express'),
