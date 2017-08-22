02b79fefe97bfe1111404a2608c1ace95d5b4c1e
deyoungster <dela.deyoungster@gmail.com>
2017-08-22 16:15:57 -0700

    Confirming sigle quote fix in Trigger values

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Sample node.js web app for Pluralsight Docker CI course
-// For demonstration purposes only. >>> Testing Auto-CI to Docker with single quotes.
+// For demonstration purposes only. >> Testing Auto-CI to Docker with single quotes. >> Final!
 'use strict';
 
 var express = require('express'),
